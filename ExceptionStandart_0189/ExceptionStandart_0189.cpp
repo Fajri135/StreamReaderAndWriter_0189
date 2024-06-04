f48dd98ee14d7bc39b7e3212905520a898c44bc0
Fajri135 <corex135@gmail.com>
2024-06-04 11:12:43 +0700

    Menampilkan pesan Awal Program, membuat fungsi try untuk pengecualian, dan catch untuk menangani pengecualian, dan menampilkan pesan baris program yang terakhir

--- ExceptionStandart_0189/ExceptionStandart_0189.cpp
+++ ExceptionStandart_0189/ExceptionStandart_0189.cpp
@@ -7,5 +7,18 @@ using namespace std;
 
 int main()
 {
-	;
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		// pesan array integer 3 elemen
+		// cout << data.at(5) << endl;*/
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		// akan dieksekusi karna array data hanya memiliki 3 elemen /
+	}
+	cout << "Baris Program yang terakhir" << endl;
+	// penanda 2: bahwa program berjalan tanpa henti meskipun terjadi kesalahan /
+	return 0;
 }
