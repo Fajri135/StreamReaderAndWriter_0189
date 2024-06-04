9e441e7356c75a7687d8af6e333365c7860c480d
Fajri135 <corex135@gmail.com>
2024-06-04 10:58:36 +0700

    Menyelesaikan int main dengan menambahkan prosedur ifstream seperti project sebelumnya ( Stream and Writer).

--- IoFileWithPath_0189/IoFileWithPath_0189.cpp
+++ IoFileWithPath_0189/IoFileWithPath_0189.cpp
@@ -32,4 +32,25 @@ int main()
     //selesai dalam menulis sekarang tutup fileny
     outfile.close();
 
+    //Membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika  ada file maka
+    if (infile.is_open()) {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
