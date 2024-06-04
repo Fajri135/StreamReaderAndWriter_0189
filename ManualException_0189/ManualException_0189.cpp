48e73b7ebcb6d02c1b4991e737573019bd06cc10
Fajri135 <corex135@gmail.com>
2024-06-04 11:03:52 +0700

    Membuat fungsi try untuk menghasilkan pengecualian

--- ManualException_0189/ManualException_0189.cpp
+++ ManualException_0189/ManualException_0189.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main()
 {
-	;
+	try {
+		cout << "selamat belajar di prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	
 }
