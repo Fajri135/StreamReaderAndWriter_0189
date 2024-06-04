85bf85734b824cee3cc546b39800994b0fd51c02
Fajri135 <corex135@gmail.com>
2024-06-04 10:57:23 +0700

    Membuat prosedur ofstream seperti di project sebelumnya

--- IoFileWithPath_0189/IoFileWithPath_0189.cpp
+++ IoFileWithPath_0189/IoFileWithPath_0189.cpp
@@ -12,4 +12,24 @@ int main()
     cout << "Masukan nama file : ";
     cin >> NamaFile;
 
+    /// membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk kesebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        // mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup fileny
+    outfile.close();
+
 }
